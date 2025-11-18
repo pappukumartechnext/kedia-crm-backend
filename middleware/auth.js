0c78dd8650cc0cea472140c714fc5f9f5657e520
pappukumartechnext <pappukumar.technext@gmail.com>
2025-11-18 15:30:27 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
@@ -24,31 +23,4 @@ const auth = async (req, res, next) => {
   }
 };
 
-=======
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
-const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1];
-    
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
-    
-    if (!user) {
-      return res.status(401).json({ message: 'User not found' });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Invalid token' });
-  }
-};
-
->>>>>>> 846c1211543b726bac61a5334e07086ffc79e154
 module.exports = auth;
